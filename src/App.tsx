d303bc4327a5260eba1b036ad7ba10616f2ba2fe
purp1eeeee <murasaki.haruki@hrbrain.co.jp>
2021-03-16 14:38:01 +0900

    add todoSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import * as React from "react";
+import * as ReactRedux from "react-redux";
+import { actions, RootState, TodoState } from "./store";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const dispatch = ReactRedux.useDispatch();
+  const { todoList } = ReactRedux.useSelector((state: RootState) => state.todo);
+
+  React.useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((json) => dispatch(actions.set(json as TodoState[])));
+  }, []);
+
+  return (
+    <ul>
+      {todoList.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
